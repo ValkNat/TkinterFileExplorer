3059d8f3cb4fe7d08379f731491b63ecb751b812
Natalie <132787852+ValkNat@users.noreply.github.com>
2023-07-16 21:03:05 -0500

    V1.1
    Adds the option of opening files directly from the app.

--- file_explorer.py
+++ file_explorer.py
@@ -3,7 +3,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
-import subprocess
+import subprocess, platform
 
 #tkinter initialization and setup
 t = Tk()
@@ -22,6 +22,8 @@ folders = []
 
 #stores item paths in root directory
 item_paths = []
+
+folder_item_paths = []
 
 #displays all files inside root directory to a listbox
 def browseRootFolder(root_folder):
@@ -44,29 +46,56 @@ def appendFolders(folder):
 def searchFolder(search_entry):
     listbox.delete(0, tk.END)
     appendFolders(root_folder)
-    print(folders)
     for item in os.listdir(root_folder):
+        item_path = os.path.join(root_folder, item)
+        item_paths.append(item_path)
         if search_entry in item.lower():
             listbox.insert(tk.END, item)
     for folder in folders:
         for item in os.listdir(folder):
+            item_path = os.path.join(folder, item)
+            folder_item_paths.append(item_path)
             if search_entry in item.lower():
                 listbox.insert(tk.END, item)
+
+def openFile(file_path):
+    if platform.system() == 'Darwin':
+        subprocess.call(('open', file_path))
+    elif platform.system() == 'Windows':
+        os.startfile(file_path)
+    else:
+        subprocess.call(('xdg-open', file_path))
+
     
 def openFileLocation(event):
     index = listbox.curselection()
-    if index:
-        directory = os.path.dirname()
+    selected_index = index[0]
+    selected_item = listbox.get(selected_index)
+    print(selected_item)
+    if os.path.basename(selected_item) in [os.path.basename(item_path) for item_path in item_paths]:
+        item_name = listbox.get(index)
+        item_path = os.path.join(root_folder, item_name)
+        open(item_path)
+    elif os.path.basename(selected_item) in [os.path.basename(folder_path) for folder_path in folder_item_paths]:
+        folder_indices = [i for i, path in enumerate(folder_item_paths) if os.path.basename(path) == os.path.basename(selected_item)]
+        item_name = listbox.get(index)
+        selected_folder_index = folder_indices[0]
+        item_path = folder_item_paths[selected_folder_index]
+        openFile(item_path)
+        
+    else:
+        print("File not found in either location!")
         
 #tkinter widget setup
 matching_items = []
 searchbox = tk.Entry()
 searchbox.pack()
-searchbox.bind("<Button-1>", openFileLocation)
 searchbox_button = tk.Button(text = "Search", command=lambda: searchFolder(searchbox.get()))
 searchbox_button.pack()
 listbox = Listbox()
 listbox.pack(fill=tk.BOTH, expand=True)
+listbox.bind("<Button-1>", openFileLocation)
+
 
 #runs tkinter and opens initial file browser for root directory
 browseRootFolder(root_folder)
